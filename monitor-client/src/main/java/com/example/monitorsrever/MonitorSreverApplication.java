510b338bc75892c8573ffe42a9c03619f9adf8a7
wanzx11 <2388824995@qq.com>
2025-04-24 20:03:13 +0800

    first commit

---
+++ monitor-client/src/main/java/com/example/monitorsrever/MonitorSreverApplication.java
@@ -0,0 +1,13 @@
+package com.example.monitorsrever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorSreverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorSreverApplication.class, args);
+	}
+
+}
---
+++ monitor-client/src/test/java/com/example/monitorsrever/MonitorSreverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monitorsrever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorSreverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
